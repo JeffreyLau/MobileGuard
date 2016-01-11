88f30e32e2851e7a2f8987031bd8a4521e8750d6
Jeffrey <meto.jeffrey@gmail.com>
2016-01-11 21:00:09 +0800

    update

--- gen/com/chaowei/mobileguard/R.java
+++ gen/com/chaowei/mobileguard/R.java
@@ -23,12 +23,14 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int splash=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080001;
+        public static final int tv_app_version=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_splash=0x7f030000;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -36,6 +38,7 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
+        public static final int app_version=0x7f050003;
         public static final int hello_world=0x7f050001;
     }
     public static final class style {
--- src/com/chaowei/mobileguard/MGApplication.java
+++ src/com/chaowei/mobileguard/MGApplication.java
@@ -1,15 +1,7 @@
 package com.chaowei.mobileguard;
 
-import java.io.IOException;
-import java.lang.ref.SoftReference;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import android.app.Application;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+
 
 /**
  * 存放共有的数据
