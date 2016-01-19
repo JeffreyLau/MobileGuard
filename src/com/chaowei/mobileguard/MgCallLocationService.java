a175d13cfe4b1c2b3dd9be5985001acadee61630
Jeffrey <meto.jeffrey@gmail.com>
2016-01-19 16:50:24 +0800

    增加歸屬地查詢功能窗體移動功能

--- src/com/chaowei/mobileguard/MgCallLocationService.java
+++ src/com/chaowei/mobileguard/MgCallLocationService.java
@@ -14,6 +14,8 @@ import android.os.IBinder;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.WindowManager.LayoutParams;
@@ -124,18 +126,32 @@ public class MgCallLocationService extends Service {
 		private TextView tv_toast_location;
 
 		// private ImageView iv_toast_location;
+		private void updateWindowLocation(int dX, int dY) {
+			params.x += dX;// 修改Ｘ軸上的位置
+			params.y += dY;// 修改Ｙ軸上的位置
+			wm.updateViewLayout(view, params);
+		}
 
 		private void windInit(Context context) {
 			wm = (WindowManager) context
 					.getSystemService(Context.WINDOW_SERVICE);
 			params = new LayoutParams();
+			// 默认view 显示在屏幕的中央　gravity　相对屏幕中央　
+			// params.x;
+			// params.y;
+			//指定參考點爲屏幕左邊上方,
+			//系統默認爲屏幕正中央
+			//params.gravity = Gravity.LEFT + Gravity.TOP;
 			params.height = WindowManager.LayoutParams.WRAP_CONTENT;
 			params.width = WindowManager.LayoutParams.WRAP_CONTENT;
 			params.format = PixelFormat.TRANSLUCENT;
-			params.type = WindowManager.LayoutParams.TYPE_TOAST;
+			// params.type = WindowManager.LayoutParams.TYPE_TOAST;
+			// 修改成系统可触摸类型的窗体,注意添加权限android.permission.SYSTEM_ALERT_WINDOW
+			params.type = WindowManager.LayoutParams.TYPE_PRIORITY_PHONE;
 			params.flags = WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
-					| WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
-					| WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE;
+					| WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
+			// |
+			// WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE;//如果需要响应触摸事件需要屏蔽掉该句
 		}
 
 		public void makeToastView(Context context, CharSequence text) {
@@ -146,9 +162,36 @@ public class MgCallLocationService extends Service {
 					.getInt(MobileGuard.SHOW_LOCATION_WHICHSTYLE, 0)]);
 			tv_toast_location = (TextView) view
 					.findViewById(R.id.tv_toast_location);
-			// iv_toast_location = (ImageView) view
-			// .findViewById(R.id.iv_toast_location);
 			tv_toast_location.setText(text);
+			view.setOnTouchListener(new View.OnTouchListener() {
+				int startX;
+				int startY;
+
+				@Override
+				public boolean onTouch(View v, MotionEvent event) {
+
+					switch (event.getAction()) {
+					case MotionEvent.ACTION_DOWN:
+						startX = (int) event.getRawX();
+						startY = (int) event.getRawY();
+						break;
+					case MotionEvent.ACTION_MOVE:
+						int newX = (int) event.getRawX();
+						int newY = (int) event.getRawY();
+						int dX = (newX - startX);
+						int dY = (newY - startY);
+						updateWindowLocation(dX, dY);
+						break;
+					case MotionEvent.ACTION_UP:
+
+						break;
+					default:
+						break;
+					}
+					return true;
+				}
+
+			});
 			wm.addView(view, params);
 		}
 
